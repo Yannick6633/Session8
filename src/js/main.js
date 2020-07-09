dd1bf9f052febab6192c70c923a80fda7227587f
Yannick6633 <yanlechaka@sfr.fr>
2020-07-10 00:35:01 +0200

    N2 commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+/*jQuery(document).ready(function($){
+    alert("okay")
+});*/
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
